7a30d5667502c985b78cc5cb091471c1e9aaf904
GN汐 <1968921855@qq.com>
2021-08-18 15:13:23 +0800

    hot-fix提交3

--- src/main/java/com/gn/App.java
+++ src/main/java/com/gn/App.java
@@ -12,5 +12,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println(1323131);
         System.out.println(1323131);
+        System.out.println(1325345351);
     }
 }

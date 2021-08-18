5cb6d8e9ad5bd1a9f2c1b4fc48599cb1beae046c
GN汐 <1968921855@qq.com>
2021-08-18 15:13:45 +0800

    master 提交1

--- src/main/java/com/gn/App.java
+++ src/main/java/com/gn/App.java
@@ -11,6 +11,5 @@ public class App
         System.out.println(11111);
         System.out.println( "Hello World!" );
         System.out.println(1323131);
-        System.out.println(1323131);
     }
 }

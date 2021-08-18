d62fbaf0908e5d9111239a5e7d6f75b3cb40c718
GN汐 <1968921855@qq.com>
2021-08-18 14:59:21 +0800

    这是我的第二次提交

---
+++ src/main/java/com/gn/App.java
@@ -0,0 +1,14 @@
+package com.gn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(11111);
+        System.out.println( "Hello World!" );
+    }
+}

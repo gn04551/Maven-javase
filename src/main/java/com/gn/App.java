78a3d2e4da61621453a739d26ca1acc100e2cee1
GN汐 <1968921855@qq.com>
2021-08-19 10:17:16 +0800

    分支合并-02

--- src/main/java/com/gn/App.java
+++ src/main/java/com/gn/App.java
@@ -11,9 +11,6 @@ public class App
         System.out.println(11111);
         System.out.println( "Hello World!" );
         System.out.println(1323131);
-        System.out.println(1323131);
-        System.out.println(1325345351);
-        System.out.println(1325345351);
         System.out.println(1325345351);
     }
 }
